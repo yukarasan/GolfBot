5cbdddb137605748f6e669ff3b145b36a45ba327
Yusuf Kara <yusufkarasan@hotmail.com>
2023-02-08 12:19:30 +0100

    The robot can drive straight, and it waves

--- main.py
+++ main.py
@@ -7,16 +7,28 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
 
-
-# Create your objects here.
+# Objects.
 ev3 = EV3Brick()
 
+left_wheel = Motor(Port.B)
+right_wheel = Motor(Port.C)
 
-# Write your program here.
-# ev3.speaker.beep(frequency = 500, duration = 1000)
-notes = ['C4/4', 'C4/4', 'G4/4', 'G4/4']
-ev3.speaker.play_notes(notes, tempo = 120)
+fork = Motor(Port.D)
+
+robot = DriveBase(left_wheel, right_wheel, wheel_diameter = 70, axle_track = 100)
+
+
+# Program.
+# notes = ['C4/4', 'C4/4', 'G4/4', 'G4/4', 'A4/4', 'A4/4', 'G4/4', 'F4/4', 'F4/4', 'E4/4', 'E4/4', 'D4/4', 'D4/4', 'C4/4']
+# ev3.speaker.play_notes(notes, tempo = 120)
+
+
+robot.straight(1000)
+
+# Wave
+fork.run_angle(speed = 2000, rotation_angle = 150)
+fork.run_angle(speed = 2000, rotation_angle = -150)
+fork.run_angle(speed = 2000, rotation_angle = 150)
+fork.run_angle(speed = 2000, rotation_angle = -150)
